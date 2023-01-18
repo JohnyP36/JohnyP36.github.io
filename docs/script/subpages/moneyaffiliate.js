b66d9f7e080e62ff49b21c63dccfab4e5002d4e5
JohnyP <81161435+JohnyP36@users.noreply.github.com>
2023-01-18 06:34:32 +0000

    Add subpage "Money/Affiliate"

---
+++ docs/script/subpages/moneyaffiliate.js
@@ -0,0 +1,16 @@
+const imageUrl = 'images\/subpages\/MoneyAffiliate\/behind.jpg';
+  function bgImgLoaded() {
+    if (!globals.headerBgImgLoaded) {
+      globals.headerBgImgLoaded = new Date().getTime();
+    } else {
+      globals.headerBgImgLoaded();
+    }
+  }
+  if (imageUrl) {
+    const img = new Image();
+    img.src = imageUrl;
+    img.onload = bgImgLoaded;
+    globals.headerBgImgExists = true;
+  } else {
+    globals.headerBgImgExists = false;
+  }
